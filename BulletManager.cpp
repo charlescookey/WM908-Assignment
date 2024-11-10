d1a36eb3ce92ac5c3f8784dff2f08ebe0a8fda9f
Charles Iroegbu <charlesiroegbu99@gmail.com>
2024-11-10 11:09:25 +0000

    Bullet Manager Class

---
+++ BulletManager.cpp
@@ -0,0 +1,48 @@
+#include "BulletManager.h"
+
+
+BulletManager::BulletManager() {
+}
+
+BulletManager::~BulletManager() {
+}
+
+void BulletManager::add(Bullet* b) {
+	bullets.add(b);
+}
+
+std::string BulletManager::SaveBullet() {
+	std::string result = "Bullets: \n";
+	for (int i = 0; i < bullets.used; i++)
+		if (bullets[i]->fired) {
+			result += bullets[i]->SaveBullet();
+		}
+	return result;
+}
+
+void BulletManager::update(Camera& cam) {
+
+	for (int i = 0; i < bullets.used; i++)
+		if (bullets[i]->fired) {
+			bullets[i]->update(cam);
+		}
+		else {
+			bullets.remove(i);
+		}
+};
+
+void BulletManager::draw(GamesEngineeringBase::Window& canvas, Camera& cam) {
+	for (int i = 0; i < bullets.used; i++)
+		if (bullets[i] != nullptr && bullets[i]->fired) {
+			cam.drawInCamera(canvas, bullets[i]->bullet);
+		}
+}
+
+Bullet& BulletManager::operator[](unsigned int index) {
+	return *bullets[index];
+}
+
+int BulletManager::used() {
+	return bullets.used;
+}
+
---
+++ BulletManager.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "Bullet.h"
+#include "GameArray.h"
+
+class BulletManager {
+	float timeElapsed = 0.f;
+	GameArray<Bullet*> bullets;
+public:
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	BulletManager();
+	//Destructor
+	~BulletManager();
+	/// <summary>
+	/// Add to manager
+	/// </summary>
+	/// <param name="b">Bullet to add</param>
+	void add(Bullet* b);
+	/// <summary>
+	/// Save bullet Outptut
+	/// </summary>
+	/// <returns>string output</returns>
+	std::string SaveBullet();
+	/// <summary>
+	/// Updates all bullets
+	/// </summary>
+	/// <param name="cam">Camera</param>
+	void update(Camera& cam);
+	/// <summary>
+	/// Draws all bullets
+	/// </summary>
+	/// <param name="canvas">Canavs to draw to</param>
+	/// <param name="cam">Camera</param>
+	void draw(GamesEngineeringBase::Window& canvas, Camera& cam);
+
+	/// <summary>
+	/// Returs the bullet at a position
+	/// </summary>
+	/// <param name="index">index of bullet to get</param>
+	/// <returns></returns>
+	Bullet& operator[](unsigned int index);
+	//returns used count of bullets
+	int used();
+
+};
