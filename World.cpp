343229556559f7d5de4dd97296c25f06b8f0fa18
Charles Iroegbu <charlesiroegbu99@gmail.com>
2024-11-10 09:48:35 +0000

    World class

---
+++ World.cpp
@@ -0,0 +1,62 @@
+#include "World.h"
+
+
+World::World() {
+}
+
+
+World::~World() {
+	for (int i = 0; i < rows; ++i) {
+		delete[] arr[i];
+	}
+}
+
+void World::load(std::string filename) {
+	std::ifstream infile(filename);
+	int tilenum;
+	std::string line;
+
+	//rows and col should be rerad from file;
+
+
+	arr = new int* [rows];
+	background = new Tile * [rows];
+	for (int i = 0; i < rows; ++i) {
+		arr[i] = new int[cols];
+		background[i] = new Tile[cols];
+	}
+	//infile >> tilenum;
+	//std::cout << tilenum << "\n";
+
+	int i = 0; int j = 0;
+	while (std::getline(infile, line)) {
+		std::stringstream ssin(line);
+		while (ssin.good() && i < cols) {
+			ssin >> arr[i][j++];
+		}
+		i++;
+		j = 0;
+	}
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			//background[i][j] = new Plane(i * 32, j * 32, filearray[arr[i][j] - 1]);
+			int _temp = arr[i][j];
+			bool canPass = _temp == 4 ? false : true;
+			ImageIndex temp = static_cast<ImageIndex>(_temp);
+			background[i][j] = Tile(i * 32, j * 32, temp, canPass);
+		}
+	}
+	infile.close();
+
+}
+//make one with vec2;
+bool World::passable(int x, int y) {
+	//add checks to enbsure no overflow
+	return background[x / 32][y / 32].ifCanPass();
+}
+
+Tile& World::at(unsigned int  X, unsigned int Y) {
+	return background[X][Y];
+}
+
---
+++ World.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "Tile.h"
+#include <fstream>
+#include <string>
+#include <sstream>
+
+class World {
+	Tile** background{ nullptr };
+public:
+	int rows = 100;
+	int cols = 100;
+
+	/// <summary>
+	/// Default Constructor
+	/// </summary>
+	World() {
+	}
+
+	int** arr;
+	/// <summary>
+	/// Default destructor
+	/// </summary>
+	~World();
+	/// <summary>
+	/// Loads a file to draw entire world
+	/// </summary>
+	/// <param name="filename">Input filename</param>
+	void load(std::string filename);
+
+	/// <summary>
+	/// checks if the tile at a position can be traversed
+	/// </summary>
+	/// <param name="x">Position X</param>
+	/// <param name="y">Position Y</param>
+	/// <returns>true if can be traversed</returns>
+	bool passable(int x, int y);
+	/// <summary>
+	/// returns the tile at a soecified position
+	/// </summary>
+	/// <param name="X">Position X</param>
+	/// <param name="Y">Position Y</param>
+	/// <returns>if can be traversed</returns>
+	Tile& at(unsigned int  X, unsigned int Y);
+
+};
