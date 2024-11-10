df4f837bd959b3a7d4d292cf8ee30280d7705fc2
Charles Iroegbu <charlesiroegbu99@gmail.com>
2024-11-10 10:02:39 +0000

    Camara Class

---
+++ Camera.cpp
@@ -0,0 +1,121 @@
+#include "Camera.h"
+
+
+Camera::Camera(int x, int y, int _width, int _height) {
+	WorldX = x;
+	WorldY = y;
+	width = _width;
+	height = _height;
+
+	//can be moved to its own function
+	gallery.loadImages("ImageNames.txt");
+
+}
+
+std::string Camera::SaveCamera() {
+	std::string result;
+	result += "Camera: " + std::to_string(WorldX) + " " + std::to_string(WorldY) + " " + std::to_string(width) + " " + std::to_string(height) + "\n";
+	return result;
+
+}
+int Camera::speed(int dir, int speed) {
+	if (dir == 0)return 0;
+	if (dir > 0)return speed;
+	return speed * -1;
+}
+void Camera::update(int x, int y, Character& main) {
+
+	if (x != 0) WorldX += speed(x, main.movespeed);
+	if (y != 0) WorldY += speed(y, main.movespeed);
+
+
+	WorldX = max(WorldX, 0);
+	WorldY = max(WorldY, 0);
+	WorldX = min(WorldX, WorldSizeX - ScreenSizeX);
+	WorldY = min(WorldY, WorldSizeY - ScreenSizeY);
+}
+
+void Camera::drawDebugPointInCameraRed(GamesEngineeringBase::Window& canvas, int X, int Y) {
+	int todrawX = X - WorldX;
+	int todrawY = Y - WorldY;
+	canvas.draw(todrawX, todrawY, 255, 0, 0);
+}
+void Camera::drawDebugPointInCameraBlue(GamesEngineeringBase::Window& canvas, int X, int Y) {
+	int todrawX = X - WorldX;
+	int todrawY = Y - WorldY;
+	canvas.draw(todrawX, todrawY, 0, 0, 255);
+}
+
+
+//Updated with Canvas
+void Camera::drawInCamera(GamesEngineeringBase::Window& canvas, Plane& p) {
+	int todrawX = p.x - WorldX;
+	int todrawY = p.y - WorldY;
+	gallery.drawAt(canvas, p.imageindex, 0, todrawX, todrawY);
+}
+
+void Camera::drawInCamera(GamesEngineeringBase::Window& canvas, Tile& p) {
+	int todrawX = p.x - WorldX;
+	int todrawY = p.y - WorldY;
+	gallery.drawAt(canvas, p.imageindex, 0, todrawX, todrawY);
+}
+
+void Camera::drawInCamera(GamesEngineeringBase::Window& canvas, Plane* p) {
+	int todrawX = p->x - WorldX;
+	int todrawY = p->y - WorldY;
+	gallery.drawAt(canvas, p->imageindex, 0, todrawX, todrawY);
+}
+
+void Camera::drawInCamera(GamesEngineeringBase::Window& canvas, Tile* p) {
+	int todrawX = p->x - WorldX;
+	int todrawY = p->y - WorldY;
+	gallery.drawAt(canvas, p->imageindex, 0, todrawX, todrawY);
+}
+
+
+
+void Camera::drawInCamera(GamesEngineeringBase::Window& canvas, Character& p) {
+	int todrawX = p.main.x - WorldX;
+	int todrawY = p.main.y - WorldY;
+	gallery.drawAt(canvas, p.main.imageindex, p.imageOffset, todrawX, todrawY);
+
+	if (p.right)
+		gallery.drawAt(canvas, p.main.imageindex, p.imageOffset, todrawX, todrawY);
+	else
+		gallery.backDrawAt(canvas, p.main.imageindex, p.imageOffset, todrawX, todrawY);
+	p.drawHealth(canvas, todrawX, todrawY - 5);
+}
+
+
+void Camera::drawBackground(GamesEngineeringBase::Window& canvas, World& world) {
+
+	int topScreenX = WorldX;
+	int topScreenY = WorldY;
+
+	int startX = WorldX / 32;
+	int startY = WorldY / 32;
+
+	//cap at 100 or size 
+	int endX = startX + (width / 32) + 1;
+	int endY = startY + (height / 32) + 1;
+
+	endX = min(world.rows, endX);
+	endY = min(world.cols, endY);
+
+	//make better
+	for (int i = startX; i < endX; i++) {//dont hard code
+		for (int j = startY; j < endY; j++) {//hardcode
+			drawInCamera(canvas, world.at(i, j));
+		}
+	}
+}
+
+bool Camera::inbounds(Plane& plane) {
+	if (plane.x < WorldX || plane.x >(WorldX + width)) {
+		return false;
+	}
+	if (plane.y < WorldY || plane.y >(WorldY + height)) {
+		return false;
+	}
+	return true;
+}
---
+++ Camera.h
@@ -0,0 +1,120 @@
+#pragma once
+#include "Gallery.h"
+#include "World.h"
+#include "Character.h"
+
+
+/////define theese inside
+#define ScreenSizeX 1024
+#define ScreenSizeY 768
+#define WorldSizeX 3200
+#define WorldSizeY 3200
+
+class Camera {
+public:
+	Gallery gallery;
+	bool moveLeft = true;
+	bool moveRight = true;
+	bool moveUp = true;
+	bool moveDown = true;
+
+	int WorldX;
+	int WorldY;
+
+	int width;
+	int height;
+
+	/// <summary>
+	/// Constructor with Speicied start positon, width and height
+	/// </summary>
+	/// <param name="x">Start X</param>
+	/// <param name="y">Start Y</param>
+	/// <param name="_width">Camera Width</param>
+	/// <param name="_height">Camera Height</param>
+	Camera(int x, int y, int _width, int _height);
+	/// <summary>
+	/// Save Camera
+	/// </summary>
+	/// <returns>String output of camera</returns>
+	std::string SaveCamera();
+	/// <summary>
+	/// Given a directiona and spped, return the speed in the specified direction
+	/// </summary>
+	/// <param name="dir">Direction</param>
+	/// <param name="speed">Speed</param>
+	/// <returns>if speed is 1 , returns -1 or 1 </returns>
+	int speed(int dir, int speed);
+	/// <summary>
+	/// Updates the main character
+	/// </summary>
+	/// <param name="x">Increment X</param>
+	/// <param name="y">incremnt Y</param>
+	/// <param name="main">Main Character</param>
+	void update(int x, int y, Character& main);
+	/// <summary>
+	/// Draws a point in the Camera frame in Blue
+	/// </summary>
+	/// <param name="canvas">canavs to draw to</param>
+	/// <param name="X">Position X</param>
+	/// <param name="Y">Position Y</param>
+	void drawDebugPointInCameraRed(GamesEngineeringBase::Window& canvas, int X, int Y);
+	/// <summary>
+	/// Draws a point in the Camera frame in Red
+	/// </summary>
+	/// <param name="canvas">canavs to draw to</param>
+	/// <param name="X">Position X</param>
+	/// <param name="Y">Position Y</param>
+	void drawDebugPointInCameraBlue(GamesEngineeringBase::Window& canvas, int X, int Y);
+
+
+	/// <summary>
+	/// Draw a Plane Reference in camera
+	/// </summary>
+	/// <param name="canvas">canavs to draw to</param>
+	/// <param name="p">Plane reference</param>
+	void drawInCamera(GamesEngineeringBase::Window& canvas, Plane& p);
+
+	/// <summary>
+	/// Draw a Tile Reference in camera
+	/// </summary>
+	/// <param name="canvas">canavs to draw to</param>
+	/// <param name="p">Tile reference</param>
+	void drawInCamera(GamesEngineeringBase::Window& canvas, Tile& p);
+
+	/// <summary>
+	/// Draw a Plane Pointer in camera
+	/// </summary>
+	/// <param name="canvas">canavs to draw to</param>
+	/// <param name="p">Plane pointer</param>
+	void drawInCamera(GamesEngineeringBase::Window& canvas, Plane* p);
+
+	/// <summary>
+	/// Draw a Tile Pointer in camera
+	/// </summary>
+	/// <param name="canvas">canavs to draw to</param>
+	/// <param name="p">Tile pointer</param>
+	void drawInCamera(GamesEngineeringBase::Window& canvas, Tile* p);
+
+
+	/// <summary>
+	/// Draw a Character Reference in camera
+	/// </summary>
+	/// <param name="canvas">canavs to draw to</param>
+	/// <param name="p">Character reference</param>
+	void drawInCamera(GamesEngineeringBase::Window& canvas, Character& p);
+
+	//test var
+	int frameSkip = 0;
+	/// <summary>
+	/// Draw a World Reference in camera
+	/// </summary>
+	/// <param name="canvas">canavs to draw to</param>
+	/// <param name="p">World reference</param>
+	void drawBackground(GamesEngineeringBase::Window& canvas, World& world);
+	/// <summary>
+	/// Check if inside camera frame
+	/// </summary>
+	/// <param name="plane">Plane to check</param>
+	/// <returns>true if inside</returns>
+	bool inbounds(Plane& plane);
+};
