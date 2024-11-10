293f20f79a246b68bf83a47048516c482fb32876
Charles Iroegbu <charlesiroegbu99@gmail.com>
2024-11-10 09:19:24 +0000

    Moved ImageIndex to its Own class

--- Plane.h
+++ Plane.h
@@ -1,12 +1,8 @@
 #pragma once
 #include <string>
 #include "Vec2.h"
+#include "ImageIndex.h"
 #include "GamesEngineeringBase.h"
-/// <summary>
-/// This hold index to load images from a text file
-/// </summary>
-enum ImageIndex { BASE = 0, INCREMENT = 1, Background = 2, CharacterCleric = 15, CharacterOrc = 25, CharacterWarrior = 35, CharcaterMinoutaur = 45, ObjectPickup = 55, ObjectBullet = 56 };
-
 
 class Plane {
 public:
@@ -80,6 +76,4 @@ public:
 	/// <param name="Y">Other Y</param>
 	/// <returns>Distance</returns>
 	float distance(int X, int Y);
-
-
 };
