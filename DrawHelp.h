8c466d5599a578581df3466c7a0d0c37ef1b5fc0
Charles Iroegbu <charlesiroegbu99@gmail.com>
2024-11-10 08:31:15 +0000

    Add comments

--- DrawHelp.h
+++ DrawHelp.h
@@ -5,7 +5,13 @@
 #include "GamesEngineeringBase.h"
 
 namespace DrawHelp {
-
+	/// <summary>
+	/// This helps draw a plain background of a specified color
+	/// </summary>
+	/// <param name="canvas">The canvas to draw to</param>
+	/// <param name="R">Red component</param>
+	/// <param name="G">Green Component</param>
+	/// <param name="B">Blue Component</param>
 	void drawPlainBackground(GamesEngineeringBase::Window& canvas, int R, int G, int  B) {
 		for (unsigned int i = 0; i < canvas.getWidth(); i++) {
 			for (unsigned int j = 0; j < canvas.getHeight(); j++) {
@@ -13,7 +19,13 @@ namespace DrawHelp {
 			}
 		}
 	}
-
+	/// <summary>
+	/// This draws an image to the screen
+	/// </summary>
+	/// <param name="canvas">The canvas to draw to</param>
+	/// <param name="image">The image to draw</param>
+	/// <param name="startX">The top left X posistion to start drawin </param>
+	/// <param name="startY">The top left X posistion to start drawing</param>
 	void drawImage(GamesEngineeringBase::Window& canvas, GamesEngineeringBase::Image& image, int startX, int startY) {
 		for (unsigned int i = 0; i < image.width; i++) {
 			for (unsigned int j = 0; j < image.height; j++) {
@@ -21,7 +33,13 @@ namespace DrawHelp {
 			}
 		}
 	}
-
+	/// <summary>
+	/// This draws an image to the screen ignoring pixels with alpha below a specified value
+	/// </summary>
+	/// <param name="canvas">The canvas to draw to</param>
+	/// <param name="image">The image to draw</param>
+	/// <param name="startX">The top left X posistion to start drawin </param>
+	/// <param name="alpha">The threshold aplha value</param>
 	void drawAlphaImage(GamesEngineeringBase::Window& canvas, GamesEngineeringBase::Image& image, int startX, int startY, int alpha) {
 		for (unsigned int i = 0; i < image.width; i++) {
 			for (unsigned int j = 0; j < image.height; j++) {
@@ -30,7 +48,14 @@ namespace DrawHelp {
 			}
 		}
 	}
-
+	/// <summary>
+	/// This draw a Filled Rectangle to the Screen
+	/// </summary>
+	/// <param name="canvas">The Canvas to drawe to</param>
+	/// <param name="rect">The rcetangle to draw</param>
+	/// <param name="R">Red component</param>
+	/// <param name="G">Green Component</param>
+	/// <param name="B">Blue Component</param>
 	void drawRectangle(GamesEngineeringBase::Window& canvas, Rect rect, int R, int G, int B) {
 		int width = static_cast<int>(rect.getWidth());
 		int height = static_cast<int>(rect.getHeight());
@@ -41,7 +66,15 @@ namespace DrawHelp {
 			}
 		}
 	}
-
+	/// <summary>
+	/// This draw a Rectangle Border to the Screen
+	/// </summary>
+	/// <param name="canvas">The Canvas to draw to</param>
+	/// <param name="rect">The rcetangle to draw</param>
+	/// <param name="border">The width of teh border to draw</param>
+	/// <param name="R">Red component</param>
+	/// <param name="G">Green Component</param>
+	/// <param name="B">Blue Component</param>
 	void drawBorder(GamesEngineeringBase::Window& canvas, Rect rect, int border, int R, int G, int B) {
 		int width = static_cast<int>(rect.getWidth());
 		int height = static_cast<int>(rect.getHeight());
@@ -59,8 +92,14 @@ namespace DrawHelp {
 		}
 	}
 
-
-	///ENSURE TO CHECK AREA DRAWN IS INSIDE
+	/// <summary>
+	/// This draw a Filled Circle to the Screen
+	/// </summary>
+	/// <param name="canvas">The Canvas to draw to</param>
+	/// <param name="circle"></param>
+	/// <param name="R">Red component</param>
+	/// <param name="G">Green Component</param>
+	/// <param name="B">Blue Component</param>
 	void drawCircle(GamesEngineeringBase::Window& canvas, Circle circle, int R, int G, int B) {
 
 		int width = static_cast<int>(circle.radius * 2);
