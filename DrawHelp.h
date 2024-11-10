24ca66805975f00b4348b4a0ce12914a589c1fc6
Charles Iroegbu <charlesiroegbu99@gmail.com>
2024-11-10 13:37:09 +0000

    moved definitions into cpp

--- DrawHelp.h
+++ DrawHelp.h
@@ -12,13 +12,7 @@ namespace DrawHelp {
 	/// <param name="R">Red component</param>
 	/// <param name="G">Green Component</param>
 	/// <param name="B">Blue Component</param>
-	void drawPlainBackground(GamesEngineeringBase::Window& canvas, int R, int G, int  B) {
-		for (unsigned int i = 0; i < canvas.getWidth(); i++) {
-			for (unsigned int j = 0; j < canvas.getHeight(); j++) {
-				canvas.draw(i, j, R, G, B);
-			}
-		}
-	}
+	void drawPlainBackground(GamesEngineeringBase::Window& canvas, int R, int G, int  B);
 	/// <summary>
 	/// This draws an image to the screen
 	/// </summary>
@@ -26,13 +20,7 @@ namespace DrawHelp {
 	/// <param name="image">The image to draw</param>
 	/// <param name="startX">The top left X posistion to start drawin </param>
 	/// <param name="startY">The top left X posistion to start drawing</param>
-	void drawImage(GamesEngineeringBase::Window& canvas, GamesEngineeringBase::Image& image, int startX, int startY) {
-		for (unsigned int i = 0; i < image.width; i++) {
-			for (unsigned int j = 0; j < image.height; j++) {
-				canvas.draw(startX + i, startY + j, image.at(i, j));
-			}
-		}
-	}
+	void drawImage(GamesEngineeringBase::Window& canvas, GamesEngineeringBase::Image& image, int startX, int startY);
 	/// <summary>
 	/// This draws an image to the screen ignoring pixels with alpha below a specified value
 	/// </summary>
@@ -40,14 +28,7 @@ namespace DrawHelp {
 	/// <param name="image">The image to draw</param>
 	/// <param name="startX">The top left X posistion to start drawin </param>
 	/// <param name="alpha">The threshold aplha value</param>
-	void drawAlphaImage(GamesEngineeringBase::Window& canvas, GamesEngineeringBase::Image& image, int startX, int startY, int alpha) {
-		for (unsigned int i = 0; i < image.width; i++) {
-			for (unsigned int j = 0; j < image.height; j++) {
-				if (image.alphaAt(i, j) > alpha)
-					canvas.draw(startX + i, startY + j, image.at(i, j));
-			}
-		}
-	}
+	void drawAlphaImage(GamesEngineeringBase::Window& canvas, GamesEngineeringBase::Image& image, int startX, int startY, int alpha);
 	/// <summary>
 	/// This draw a Filled Rectangle to the Screen
 	/// </summary>
@@ -56,16 +37,7 @@ namespace DrawHelp {
 	/// <param name="R">Red component</param>
 	/// <param name="G">Green Component</param>
 	/// <param name="B">Blue Component</param>
-	void drawRectangle(GamesEngineeringBase::Window& canvas, Rect rect, int R, int G, int B) {
-		int width = static_cast<int>(rect.getWidth());
-		int height = static_cast<int>(rect.getHeight());
-
-		for (unsigned int i = 0; i < width; i++) {
-			for (unsigned int j = 0; j < height; j++) {
-				canvas.draw(rect.start.x + i, rect.start.y + j, R, G, B);
-			}
-		}
-	}
+	void drawRectangle(GamesEngineeringBase::Window& canvas, Rect rect, int R, int G, int B);
 	/// <summary>
 	/// This draw a Rectangle Border to the Screen
 	/// </summary>
@@ -75,22 +47,7 @@ namespace DrawHelp {
 	/// <param name="R">Red component</param>
 	/// <param name="G">Green Component</param>
 	/// <param name="B">Blue Component</param>
-	void drawBorder(GamesEngineeringBase::Window& canvas, Rect rect, int border, int R, int G, int B) {
-		int width = static_cast<int>(rect.getWidth());
-		int height = static_cast<int>(rect.getHeight());
-
-		float _border = static_cast<float>(border);
-		Vec2 start = rect.start + Vec2(_border);
-
-		Rect inside(start, width - (border * 2), height - (border * 2));
-
-		for (unsigned int i = 0; i < width; i++) {
-			for (unsigned int j = 0; j < height; j++) {
-				if (!inside.inside(rect.start.x + i, rect.start.y + j))
-					canvas.draw(rect.start.x + i, rect.start.y + j, R, G, B);
-			}
-		}
-	}
+	void drawBorder(GamesEngineeringBase::Window& canvas, Rect rect, int border, int R, int G, int B);
 
 	/// <summary>
 	/// This draw a Filled Circle to the Screen
@@ -100,21 +57,5 @@ namespace DrawHelp {
 	/// <param name="R">Red component</param>
 	/// <param name="G">Green Component</param>
 	/// <param name="B">Blue Component</param>
-	void drawCircle(GamesEngineeringBase::Window& canvas, Circle circle, int R, int G, int B) {
-
-		int width = static_cast<int>(circle.radius * 2);
-		int height = static_cast<int>(circle.radius * 2);
-
-		Vec2 start = circle.getStartVec();
-
-		for (unsigned int i = 0; i < width; i++) {
-			for (unsigned int j = 0; j < height; j++) {
-				if (circle.inside(Vec2(start.x + i, start.y + j)))
-					canvas.draw(start.x + i, start.y + j, R, G, B);
-			}
-		}
-
-	}
-
-
+	void drawCircle(GamesEngineeringBase::Window& canvas, Circle circle, int R, int G, int B);
 }
